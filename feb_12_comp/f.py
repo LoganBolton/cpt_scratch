79e676360fe0740e6eccecb6c6a4f292645a9c76
Log <loganbolton101@gmail.com>
2026-02-15 05:08:59 -0600

    progress

--- feb_12_comp/f.py
+++ feb_12_comp/f.py
@@ -1,4 +1,5 @@
 import heapq
+import uuid
 # send one at a time through shortest dist graph. every time, remove a capacity from that node.
 # When node reaches 0, rebuild shortest dist graph
 
@@ -9,9 +10,11 @@ for i in range(n):
 
 for _ in range(m):
     u, v, c, w = map(int, input().split())
-    graph[u].append((v,c,w))
+    id = uuid.uuid4().hex
 
-print(graph)
+    graph[u].append((v,c,w,id))
+
+# print(graph)
 
 def dijkstra(graph, start):
     dist = {node: float('inf') for node in graph}
@@ -24,45 +27,84 @@ def dijkstra(graph, start):
         if curr_dist > dist[node]: #not closest
             continue
 
-        for neighbor, capacity, cost in graph[node]:
+        for neighbor, capacity, cost, id in graph[node]:
             new_dist = curr_dist + cost 
             if new_dist < dist[neighbor]:
                 dist[neighbor] = new_dist
                 prev[neighbor] = node
                 heapq.heappush(pq,(new_dist, neighbor))
-    print(dist)
+    print(f"graph: {graph}")
+    print(f"dist: {dist}")
+    print(f"prev: {prev}")
     return dist, prev
 
 short, prev = dijkstra(graph, s)
-max_flow = -1
 total_cost = 0
-print(prev)
+# print(prev)
 
 # go backwards to figure out path
-def dfs(node, short, prev, max_flow, total_cost):
+def dfs(node, short, prev, curr_flow, total_cost, deleted, path):
+    # print(node, total_cost, deleted)
+    print(node, total_cost, curr_flow, path)
+    # path.append(node)
     if node == s:
-        return
+        for node, idx in deleted:
+            # print(deleted, delete)
+            # print(node, idx, deleted)
+            del graph[node][idx] 
+        if len(deleted) > 0:
+            # print(deleted)
+            short, prev = dijkstra(graph, s)
+
+        return curr_flow, total_cost, short, prev, deleted, path
 
     prev_n = prev[node]
     edges = graph[prev_n]
 
     for i, edge in enumerate(edges):
-        v, capacity, cost = edge
+        v, capacity, cost, id = edge
 
         if v == node:
-            max_flow = max(capacity, max_flow)
+            # print((v,capacity, cost))
+            curr_flow = max(capacity, curr_flow)
             total_cost += cost       
             new_capacity = capacity -1
 
-            edges[i] = (v, new_capacity, cost)
+            edges[i] = (v, new_capacity, cost, id)
+
+            path_copy = path.copy()
 
             if new_capacity == 0: # remove branch, remake graph
-                print(f"Deleting {edges[i]}")
-                del edges[i]
-                short, prev = dijkstra(graph, s)
-                
-            max_flow, total_cost = dfs(prev[prev_n], short, prev, max_flow, total_cost)
-    return max_flow, total_cost
+                # print(f"Deleting {graph[prev_n][i], prev_n, i}")
+                future_delete = (prev_n, i) 
+                deleted.append(future_delete)
 
-max_flow, total_cost = dfs(t, short, prev, max_flow, total_cost)
+            # print(node, v)
+            if prev_n is not None:    
+                curr_flow, total_cost, short, prev, deleted, path = dfs(prev_n, short, prev, curr_flow, total_cost, deleted, path_copy+[id])
+
+    return curr_flow, total_cost, short, prev, deleted, path
+
+seen = set()
+
+max_flow = 0
+two_hop_not_taken = True
+while short[t] != float('inf'): 
+    min_flow, curr_cost, short, prev, deleted, path = dfs(t, short, prev, 0, 0, [], [])
+
+    new_path = True
+    # print("-----")
+    # print(path) 
+    for edge in path:
+        if edge in seen:
+            new_path = False
+            break
+        seen.add(edge)
+    if new_path:
+        # print("new path", path, min_flow)
+        max_flow += min_flow
+
+    total_cost += curr_cost
+
+# print("----------")
 print(max_flow, total_cost)
