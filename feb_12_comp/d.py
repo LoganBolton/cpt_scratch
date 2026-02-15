d65a309ac232c32a5b81d17556ee51087f744347
Log <loganbolton101@gmail.com>
2026-02-15 01:59:39 -0600

    solve d

--- feb_12_comp/d.py
+++ feb_12_comp/d.py
@@ -3,6 +3,9 @@
 # if middle, append te back of front deque
 # if bcak, append to end of last deque
 
+#maybe enforce that both arrays have to be basically the same size?
+# if you want to add to middle and second array is 1 bigger, move one to the left and add to the top
+
 # 9
 # push_back 9
 # push_front 3
@@ -16,52 +19,79 @@
 
 
 # 3 5   9
-# 
+# 3 5   1 9
 
+
+#       1
 
 from collections import deque
+import sys
+
+def balance_bias(first, last):
+    # right side bigger bias
+    if len(first) - len(last) == 0:
+        return
+    if len(first)-len(last) > 0: # left is larger
+        prev_middle = first.pop()
+        last.appendleft(prev_middle)
+    if len(last) - len(first) > 1: # right is too large
+        prev_middle = last.popleft()
+        first.append(prev_middle)
+
+def push_front(first, val):
+    first.appendleft(val)
+
+def push_middle(first,last,val):
+    if len(first) < len(last): # if left smaller, move last front to first back and add val to last front
+        prev_middle = last.popleft()
+        first.append(prev_middle)
+        last.appendleft(val)
+    elif len(first) == len(last):
+        last.appendleft(val)
+    
+    first = first
+    last = last
+    
+
+def push_back(last, val):
+    last.append(val)
+
+# 2
+def get_i(first,last,i):
+    if i < len(first): # ex: idx 0 is less than size 1
+        return first[i]
+    else:
+        prev_idxs = len(first)
+        return last[i-prev_idxs]
+
+data = sys.stdin.buffer.read().split()
+it = iter(data)
 
 first = deque()
 last = deque()
+n = int(next(it))
 
-
-def push_front(first, last, val):
-    first.appendleft(val)
-    return first, last
-
-def push_middle(first,last,val):
-    if len(first) == 0 or (len(first) + len(last)) % 2 != 0: # odd:
-        first.append(val)
-    else:
-        prev_middle = first.pop()
-        first.append(val)
-        last.appendleft(prev_middle)
-    return first, last
-
-def push_back(first, last, val):
-    last.append(val)
-    return first, last
-
-def get_i(first,last,i):
-    if i < len(first)-1:
-        return first[i]
-    else:
-        return last[len(first)-1-i]
-
-n = int(input())
+out = bytearray()
 
 for _ in range(n):
-    line = input()
-    command, value = line.split(' ')
-    print(command, value)
-    if command == "push_back": 
-        push_back(first, last, value)
-    if command == "push_front": 
-        push_front(first, last, value)
-    if command == "push_middle": 
+    command = next(it)
+    value = next(it)
+
+    # line = input()
+    # command, value = line.split(' ')
+    if command == b'push_back': 
+        push_back(last, value)
+        balance_bias(first, last)
+    if command == b'push_front': 
+        push_front(first, value)
+        balance_bias(first, last)
+    if command == b'push_middle': 
         push_middle(first, last, value)
-    if command == "get": 
-        print(get_i(first, last, int(value)))
-    print(first, last)
-    print()
-    
+        balance_bias(first, last)
+    if command == b'get': 
+        out += get_i(first, last, int(value)) + b'\n'
+    # print(command, value)
+    # print(first, last)
+    # print()
+
+sys.stdout.buffer.write(out)
